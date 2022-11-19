5f09f7b4f29be5cf6400f8f61729aae018806b19
AntonSimonov94 <79451063+AntonSimonov94@users.noreply.github.com>
2022-11-19 17:41:15 +0300

    hw1

---
+++ index.js
@@ -0,0 +1,50 @@
+import colors from 'colors';
+
+
+const [start, end] = process.argv.splice(2);
+
+
+const simpleNumber = (rangeStart, rangeEnd) => {
+    let arr = [];
+    for (let i = Number(rangeStart); i <= Number(rangeEnd); i++) {
+        let n = 0;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                n = 0;
+                break
+            } else n = 1
+        }
+        if (n === 1) arr.push(i);
+    }
+    return arr
+}
+
+const changeColors = (array) => {
+    let i = 1;
+    array.forEach((num) => {
+        switch (i) {
+            case 1: {
+                console.log(colors.green(num));
+                i++;
+                break;
+            }
+            case 2: {
+                console.log(colors.yellow(num));
+                i++;
+                break;
+            }
+            case 3: {
+                console.log(colors.red(num));
+                i = 1;
+                break;
+            }
+        }
+    })
+}
+
+if ((start) && (end) && (Number(end) >= Number(start))) {
+    let arrNumber = simpleNumber(start, end);
+    if (arrNumber.length === 0) {
+        console.log('В данном диапозоне простых чисел нет')
+    } else changeColors(arrNumber)
+} else console.log('Неверный диапозон чисел')
