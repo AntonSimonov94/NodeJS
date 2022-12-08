0674a9dc5dc703df4fb8eb2b5431b38397b49320
unknown <aasimonov1994@mail.ru>
2022-12-08 12:18:37 +0300

    hw6

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-import fsp from "fs/promises";
-import path from "path";
-import http from "http"
+import http from "http";
+
 import fs from "fs";
+import path from "path";
+import { Server } from "socket.io";
 
 const host = "localhost";
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-    if (request.method === "GET") {
-        const url = request.url.split("?")[0];
-        const lastPath = path.join(process.cwd(), url);
 
-        fs.stat(lastPath, (error, stats) => {
-            if (error) {
-                console.log(error)
-            } else {
-                if (stats.isDirectory(lastPath)) {
-                    fsp
-                        .readdir(lastPath)
-                        .then((list) => {
-                            list.forEach((link) => {
-                                response.write(`<h1><a href=${path.join(url, link)}>${link}</a></h1>`, 'utf-8')
-                            })
-                            response.end()
-                        })
-                } else {
-                    const readStream = fs.createReadStream(lastPath);
-                    readStream.pipe(response)
-                }
-            }
-        })
+const server = http.createServer((req, res) => {
+    if (["GET", "POST", "PUT"].includes(req.method)) {
+
+        const filePath = path.join(process.cwd(), "./index.html");
+        const rs = fs.createReadStream(filePath);
+
+        rs.pipe(res);
     }
+});
+let clientID = [];
+const io = new Server(server)
+io.on('connection', (client) => {
+    //clientID.push(client.id)
+    //console.log(clientID)
+    console.log('Websocket connected')
+
+    client.on('client-msg', (data) => {
+        client.broadcast.emit('server-msg', { msg: data.msg })
+        client.emit('server-msg', { msg: data.msg })
+    })
+
+    client.on('disconnect', (client) => {
+        console.log('Websocket disconnection')
+    })
+
+    client.on('reconnect', () => {
+        console.log('Websocket reconnect')
+    });
 })
 
+
+
 server.listen(port, host, () =>
-    console.log(`Server running at http://${host}:${port}`))
+    console.log(`Server running at http://${host}:${port}`)
+);
